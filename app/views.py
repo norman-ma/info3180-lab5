2f4caf900b74792740c1268ffdc1a54f7b06014a
Michael Anthony Norman <michaelanthony.norman@mymona.uwi.edu>
2017-02-27 00:12:16 +0000

    step 9

--- app/views.py
+++ app/views.py
@@ -29,23 +29,38 @@ def about():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
-    if request.method == "POST":
+    if request.method == "POST" and form.validate_on_submit():
         # change this to actually validate the entire form submission
         # and not just one field
         if form.username.data:
             # Get the username and password values from the form.
-
+            u = form.username.data
+            p = form.password.data
             # using your model, query database for a user based on the username
             # and password submitted
             # store the result of that query to a `user` variable so it can be
             # passed to the login_user() method.
-
+            user = UserProfile.query.filter_by(username=u, password=p).first()
             # get user id, load into session
-            login_user(user)
+            if user is not None:
+                login_user(user)
 
-            # remember to flash a message to the user
-            return redirect(url_for("home")) # they should be redirected to a secure-page route instead
+                # remember to flash a message to the user
+                flash("Login successful")
+                return redirect(url_for("secure_page")) # they should be redirected to a secure-page route instead
+            else:
+                flash('Username or Password is incorrect.', 'danger')
+
     return render_template("login.html", form=form)
+
+@app.route("/secure-page")
+@login_required
+def secure_page():
+    if not current_user.is_authenticated:
+        flash("Please log in to continue")
+        return redirect(url_for("login.html"))
+    else:
+        return render_template("secure-page.html")
 
 # user_loader callback. This callback is used to reload the user object from
 # the user ID stored in the session
